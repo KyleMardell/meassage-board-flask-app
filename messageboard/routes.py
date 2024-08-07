757eb22e457a9094a5ecdc7ab15de69860c93303
Kyle Mardell <kyleslufc@gmail.com>
2024-08-07 21:38:54 +0000

    Create initial index and login pages, set up routes.

--- messageboard/routes.py
+++ messageboard/routes.py
@@ -4,4 +4,9 @@ from messageboard import app, db
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
